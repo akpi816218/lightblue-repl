a32c6594491b532234e80a31aed7634440d445b4
Potato Land <96445998+potatoland4492@users.noreply.github.com>
2022-07-20 08:31:32 -0700

    Create list.py

---
+++ list.py
@@ -0,0 +1,16 @@
+import glob
+print('Collecting files...')
+html = glob.glob('./' + '/**/*.html', recursive=True)
+css = glob.glob('./' + '/**/*.css', recursive=True)
+js = glob.glob('./' + '/**/*.js', recursive=True)
+svg = glob.glob('./' + '/**/*.svg', recursive=True)
+ico = glob.glob('./' + '/**/*.ico', recursive=True)
+wav = glob.glob('./' + '/**/*.wav', recursive=True)
+mp3 = glob.glob('./' + '/**/*.mp3', recursive=True)
+# ogg = glob.glob('./' + '/**/*.ogg', recursive=True)
+print('Writing...')
+split = "',\n'"
+list = open('list.txt', 'w')
+list.write("[\n'"+split.join(html)+split+split.join(css)+split+split.join(js)+split+split.join(svg)+split+split.join(ico)+split+split.join(wav)+split+split.join(mp3)+'\n]')
+list.close()
+print('Done.')
