556b7713cd69445fc6f5731bcd125e4c39cfbbf7
Potato Land <potatoland4492@gmail.com>
2022-07-22 20:06:57 -0700

    fix???

--- push.sh
+++ push.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 ts=$(date -u +"%m-%d-%Y %H:%M:%S")
+token=${{secrets.GITHUB_TOKEN}}
 git config http.sslVerify false
 git config --local user.email "github-actions[bot]@users.noreply.github.com"
 git config --local user.name "github-actions[bot]"
 git add -A
 git commit -m "Automated push at ${ts}"
-git push "https://${GITHUB_ACTOR}:${{secrets.GITHUB_TOKEN}}@github.com/${GITHUB_REPOSITORY}.git"
+git push "https://${GITHUB_ACTOR}:${token}@github.com/${GITHUB_REPOSITORY}.git"
 exit 0
