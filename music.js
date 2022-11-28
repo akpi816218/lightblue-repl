5e4cccd712156f054f3e4d9aa27fafe66417481f
akpi816218 <111009970+akpi816218@users.noreply.github.com>
2022-11-28 10:23:00 -0800

    Update music.js

--- music.js
+++ music.js
@@ -3,7 +3,7 @@ let audio = document.querySelector('audio');
 function play() {
 	audio.muted = false;
 	audio.currentTime = 0;
-	setTimeout(audio.play(), 1000);
+	setTimeout(() => { audio.play() }, 1000);
 	document.removeEventListener('keypress', play());
 }
 
